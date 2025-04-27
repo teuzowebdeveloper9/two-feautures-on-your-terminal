3a66c97bf9aeb9160b312045b2250e76e42990f4
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-04-27 18:53:40 -0300

    tested and validated prompt functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import prompt from "prompt";
 
-async function main() {
-    
-}
+import {promptConfig} from "./prompts/prompt-main.js";
+
+prompt.start();
+
+prompt.get(promptConfig, (err, choose) => {
+  if (err) {
+    console.error("Erro ao obter entrada:", err);
+    return;
+  }
+  if (choose.prompt == 1) {
+    console.log("You have chosen QR code generator");
+  } else {
+    console.log("You have chosen password generator");
+  }
+});  
---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,13 @@
+
+
+export const promptConfig = [
+    {
+        name: "prompt",
+        description: "welcome to my project  choose a tool (1 qr code generator or (2 PASSWORD",
+        pattern: /^[1-2]+$/,
+        message: "Please enter a number between 1 and 2",
+        required: true,
+
+    }
+]
+

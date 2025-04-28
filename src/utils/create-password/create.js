200e9dee6096be76e686e702239d719da7717aab
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-04-27 23:30:38 -0300

    finish

--- src/utils/create-password/create.js
+++ src/utils/create-password/create.js
@@ -1,11 +1,11 @@
 import chalk from "chalk";
 import { createPassword } from "./handle.js";
-import dotenv from "dotenv";
+
 
 export async function callPassword() {
     console.log(chalk.green("password generator"));
     await createPassword().then((password) => {
-        console.log(`your password is: ${password}`);
+        console.log(chalk.greenBright(`your password is: ${password}`));
         console.log(chalk.greenBright("password generated successfully"));
     });
 }
